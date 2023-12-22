4f579391b1a227bab632f16e76600e28878fd4c1
tuhinhossain123 <csetuhin55@gmail.com>
2023-12-22 16:29:46 +0600

    add todo data in database

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+ 
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.taymcgi.mongodb.net/?retryWrites=true&w=majority`;
@@ -25,6 +26,22 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database= client.db("taskManagement");
+    const taskCollection  = database.collection("task");
+
+    app.get('/task', async(req, res)=>{
+        const result = taskCollection.find().toArray();
+        res.send(result)
+    })
+
+    app.post('/task', async(req, res)=>{
+        const task = req.body;
+        const result = await taskCollection.insertOne(task);
+        res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
